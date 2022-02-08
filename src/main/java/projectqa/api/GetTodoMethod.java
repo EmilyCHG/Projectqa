c81f81f2c7b841b68becb8c3369b9b0441f1ef96
emy06 <emy0608ch@gmail.com>
2022-02-08 09:49:51 -0300

    test project

--- src/main/java/projectqa/api/GetTodoMethod.java
+++ src/main/java/projectqa/api/GetTodoMethod.java
@@ -1,26 +1,18 @@
-
 package projectqa.api;
 
 import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
 import com.qaprosoft.carina.core.foundation.api.annotation.Endpoint;
-import com.qaprosoft.carina.core.foundation.api.annotation.RequestTemplatePath;
 import com.qaprosoft.carina.core.foundation.api.annotation.ResponseTemplatePath;
 import com.qaprosoft.carina.core.foundation.api.annotation.SuccessfulHttpStatus;
 import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
 import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
 
-@RequestTemplatePath(path = "api/user/post/rq.json")
-@ResponseTemplatePath(path = "api/user/post/rs.json")
-@Endpoint(methodType = HttpMethodType.POST, url = "https://reqres.in/api/users")
+@ResponseTemplatePath(path = "api/user/get/rs.json")
+@Endpoint(methodType = HttpMethodType.GET, url = "https://jsonplaceholder.typicode.com/todos")
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
-public class PostUserMethod extends AbstractApiMethodV2 {
+public class GetTodoMethod extends AbstractApiMethodV2 {
 
-    public PostUserMethod(){
-     super();
-     setProperties("api/user/post/user.properties");
-
+    public GetTodoMethod(){
+        super();
     }
-
-
-
 }
--- src/main/java/projectqa/api/PostTodoMethod.java
+++ src/main/java/projectqa/api/PostTodoMethod.java
@@ -1,18 +1,26 @@
+
 package projectqa.api;
 
 import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
 import com.qaprosoft.carina.core.foundation.api.annotation.Endpoint;
+import com.qaprosoft.carina.core.foundation.api.annotation.RequestTemplatePath;
 import com.qaprosoft.carina.core.foundation.api.annotation.ResponseTemplatePath;
 import com.qaprosoft.carina.core.foundation.api.annotation.SuccessfulHttpStatus;
 import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
 import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
 
-@ResponseTemplatePath(path = "api/user/get/rs.json")
-@Endpoint(methodType = HttpMethodType.GET, url = "https://jsonplaceholder.typicode.com/todos")
+@RequestTemplatePath(path = "api/user/post/rq.json")
+@ResponseTemplatePath(path = "api/user/post/rs.json")
+@Endpoint(methodType = HttpMethodType.POST, url = "https://reqres.in/api/users")
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
-public class GetUserMethod extends AbstractApiMethodV2 {
+public class PostTodoMethod extends AbstractApiMethodV2 {
 
-    public GetUserMethod(){
-        super();
+    public PostTodoMethod(){
+     super();
+     setProperties("api/user/post/user.properties");
+
     }
+
+
+
 }
